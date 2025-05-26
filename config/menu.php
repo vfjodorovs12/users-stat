13d9bc3533f10de3a345c26f1ae7658dd2c272ba
Clive Scott <131658606+vfjodorovs12@users.noreply.github.com>
2025-05-26 20:44:59 +0100

    Create menu.php
    <?php
    
    return [
        'users-stat' => [
            'name' => 'Статистика пользователей',
            'route' => 'users-stat.pilots-stats',
            'icon' => 'fa fa-users', // любой FontAwesome
            'permission' => 'seatusers-stat.view', // настрой по необходимости
            'children' => [], // можно добавить подпункты
        ],
    ];

---
+++ config/menu.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'users-stat' => [
+        'name' => 'Статистика пользователей',
+        'route' => 'users-stat.pilots-stats',
+        'icon' => 'fa fa-users', // любой FontAwesome
+        'permission' => 'seatusers-stat.view', // настрой по необходимости
+        'children' => [], // можно добавить подпункты
+    ],
+];
