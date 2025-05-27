e088a0d307fe47715768105ae543d0c620cf2709
Clive Scott <131658606+vfjodorovs12@users.noreply.github.com>
2025-05-27 14:08:00 +0100

    Add files via upload

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use Vfjodorovs12\UsersStat\Http\Controllers\UsersStatController;
 
-Route::group(['namespace' => 'Vfjodorovs12\UsersStat\Http\Controllers', 'middleware' => ['web', 'auth']], function () {
-    Route::get('/users-stat/pilots-stats', [UsersStatController::class, 'index'])->name('users-stat.pilots-stats');
+Route::group(['middleware' => ['web']], function () {
+    Route::get('/users-stat', [UsersStatController::class, 'index'])
+        ->name('usersstat.index');
 });
