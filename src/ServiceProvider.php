ea8e3954f2dfae85015e7eb02574ed11baf21c09
Clive Scott <131658606+vfjodorovs12@users.noreply.github.com>
2025-05-27 21:50:24 +0100

    Update ServiceProvider.php
    <?php
    
    namespace Vfjodorovs12\UsersStat;
    
    use Illuminate\Support\ServiceProvider as BaseServiceProvider;
    
    class ServiceProvider extends BaseServiceProvider
    {
        public function boot()
        {
            // Подключение маршрутов, шаблонов и миграций
            $this->loadRoutesFrom(__DIR__.'/../routes/web.php');
            $this->loadViewsFrom(__DIR__.'/../resources/views', 'usersstat');
            $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
    
            // Регистрируем пункт меню в Eve Seat
            $this->app->booted(function () {
                if (app()->bound('seat.menu')) {
                    app('seat.menu')->add([
                        'name' => 'Статистика пилотов',
                        'icon' => 'fa fa-users',
                        'route' => 'usersstat.index', // Имя вашего маршрута в routes/web.php
                        'permission' => null, // Можно указать нужные права доступа или оставить null
                    ]);
                }
            });
        }
    
        public function register()
        {
            //
        }
    }

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -8,12 +8,26 @@ class ServiceProvider extends BaseServiceProvider
 {
     public function boot()
     {
+        // Подключение маршрутов, шаблонов и миграций
         $this->loadRoutesFrom(__DIR__.'/../routes/web.php');
         $this->loadViewsFrom(__DIR__.'/../resources/views', 'usersstat');
         $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
+
+        // Регистрируем пункт меню в Eve Seat
+        $this->app->booted(function () {
+            if (app()->bound('seat.menu')) {
+                app('seat.menu')->add([
+                    'name' => 'Статистика пилотов',
+                    'icon' => 'fa fa-users',
+                    'route' => 'usersstat.index', // Имя вашего маршрута в routes/web.php
+                    'permission' => null, // Можно указать нужные права доступа или оставить null
+                ]);
+            }
+        });
     }
 
     public function register()
     {
+        //
     }
 }
