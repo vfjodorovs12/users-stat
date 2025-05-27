050affdb731ce262da601791a9b995823be337b2
Clive Scott <131658606+vfjodorovs12@users.noreply.github.com>
2025-05-27 14:11:46 +0100

    Update Pilot.php

--- src/Model/Pilot.php
+++ src/Model/Pilot.php
@@ -1,1 +1,17 @@
+<?php
 
+namespace Vfjodorovs12\UsersStat\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Pilot extends Model
+{
+    protected $table = 'corporation_pilots';
+    protected $fillable = ['character_id', 'character_name'];
+    public $timestamps = false;
+
+    public function fleetStats()
+    {
+        return $this->hasMany(FleetAttendance::class, 'character_id', 'character_id');
+    }
+}
