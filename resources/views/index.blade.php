bf73d455e34dbeba630d40502779d024e75762e3
Clive Scott <131658606+vfjodorovs12@users.noreply.github.com>
2025-05-28 04:17:26 +0100

    Update index.blade.php
    @extends('web::layouts.grids.12')
    
    @section('title', 'Статистика пилотов')
    @section('page_header', 'Статистика пилотов')
    @section('page_description')
        Все пилоты корпорации<br>
        и статистика посещения флотов
    @endsection
    
    @section('full')
        <div class="card">
            <div class="card-header">
                <h3 class="card-title">Все пилоты корпорации и статистика посещения флотов</h3>
            </div>
            <div class="card-body table-responsive">
                <table class="table table-bordered table-hover">
                    <thead>
                        <tr>
                            <th>Имя пилота</th>
                            <th>ID персонажа</th>
                            <th>Флотов посещено</th>
                        </tr>
                    </thead>
                    <tbody>
                        @foreach($pilots as $pilot)
                            <tr>
                                <td>{{ $pilot->character_name }}</td>
                                <td>{{ $pilot->character_id }}</td>
                                <td>{{ $pilot->fleet_stats_count }}</td>
                            </tr>
                        @endforeach
                    </tbody>
                </table>
            </div>
        </div>
    @endsection

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,28 +1,36 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Статистика пилотов</title>
-    <style>
-        table { border-collapse: collapse; width: 70%; }
-        th, td { border: 1px solid #aaa; padding: 5px 10px; }
-        th { background: #eee; }
-    </style>
-</head>
-<body>
-    <h1>Все пилоты корпорации и статистика посещения флотов</h1>
-    <table>
-        <tr>
-            <th>Имя пилота</th>
-            <th>ID персонажа</th>
-            <th>Флотов посещено</th>
-        </tr>
-        @foreach($pilots as $pilot)
-            <tr>
-                <td>{{ $pilot->character_name }}</td>
-                <td>{{ $pilot->character_id }}</td>
-                <td>{{ $pilot->fleet_stats_count }}</td>
-            </tr>
-        @endforeach
-    </table>
-</body>
-</html>
+@extends('web::layouts.grids.12')
+
+@section('title', 'Статистика пилотов')
+@section('page_header', 'Статистика пилотов')
+@section('page_description')
+    Все пилоты корпорации<br>
+    и статистика посещения флотов
+@endsection
+
+@section('full')
+    <div class="card">
+        <div class="card-header">
+            <h3 class="card-title">Все пилоты корпорации и статистика посещения флотов</h3>
+        </div>
+        <div class="card-body table-responsive">
+            <table class="table table-bordered table-hover">
+                <thead>
+                    <tr>
+                        <th>Имя пилота</th>
+                        <th>ID персонажа</th>
+                        <th>Флотов посещено</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach($pilots as $pilot)
+                        <tr>
+                            <td>{{ $pilot->character_name }}</td>
+                            <td>{{ $pilot->character_id }}</td>
+                            <td>{{ $pilot->fleet_stats_count }}</td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>
+@endsection
