ba1a297ea514ad5d50439643bfa17c9971141154
Clive Scott <131658606+vfjodorovs12@users.noreply.github.com>
2025-05-28 03:56:15 +0100

    Update usersstat.sidebar.php

--- src/Config/usersstat.sidebar.php
+++ src/Config/usersstat.sidebar.php
@@ -1,11 +1,10 @@
 <?php
-
 return [
-    'users-stat' => [
-        'name' => 'Статистика пользователей',
-        'route' => 'users-stat.pilots-stats',
-        'icon' => 'fa fa-users', // любой FontAwesome
-        'permission' => 'seatusers-stat.view', // настрой по необходимости
-        'children' => [], // можно добавить подпункты
+    'usersstat' => [
+        'name'          => 'Статистика пилотов',
+        'icon'          => 'fa fa-users',
+        'route'         => 'usersstat.index',
+        'route_segment' => 'users-stat', // <--- ОБЯЗАТЕЛЬНО!
+        'permission'    => null,
     ],
 ];
