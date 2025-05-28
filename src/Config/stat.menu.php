4f1c1c60a212159550e16977c1b0e7286548b4c5
Clive Scott <131658606+vfjodorovs12@users.noreply.github.com>
2025-05-28 03:45:24 +0100

    Create stat.menu.php
    <?php
    return [
        'usersstat' => [
            'name' => 'Статистика пилотов',
            'icon' => 'fa fa-users',
            'route' => 'usersstat.index',
            'permission' => null,
        ],
    ];

---
+++ src/Config/stat.menu.php
@@ -0,0 +1,10 @@
+
+<?php
+return [
+    'usersstat' => [
+        'name' => 'Статистика пилотов',
+        'icon' => 'fa fa-users',
+        'route' => 'usersstat.index',
+        'permission' => null,
+    ],
+];
